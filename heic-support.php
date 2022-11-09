a04c3c4e6fa10c64b2960c08111566b7a8740a64
Corey Salzano <corey.salzano@gmail.com>
2022-11-09 18:57:02 -0500

    Adds heic to the allowed upload mime types if needed. Adds the promise on the tin, creates .webp copies of .heic files uploaded to the Media Library.

--- heic-support.php
+++ heic-support.php
@@ -15,6 +15,73 @@ defined( 'ABSPATH' ) || exit;
  * Text-domain: heic-support
  */
 
+// CORE FUNCTIONALITY.
+
+/**
+ * Allow .heic files to be uploaded into the Media Library.
+ *
+ * @param  array            $mimes Array of allowed mime types.
+ * @param  int|WP_User|null $user The current user.
+ * @return array
+ */
+function heic_support_add_mimes( $mimes, $user ) {
+	if ( empty( $mimes['heic'] ) ) {
+		$mimes['heic'] = 'image/heic';
+	}
+	return $mimes;
+}
+add_filter( 'upload_mimes', 'heic_support_add_mimes', 10, 2 );
+
+/**
+ * Filter callback on add_attachment. Creates a .webp copy of .heic images
+ * uploaded to the Media Library.
+ *
+ * @param  int $post_id The ID of a new attachment.
+ * @return void
+ */
+function heic_support_create_webp_copy( $post_id ) {
+	// Is ImageMagick running?
+	if ( ! class_exists( 'Imagick' ) ) {
+		// No.
+		return;
+	}
+	$file_path = get_attached_file( $post_id );
+	if ( false === $file_path ) {
+		return;
+	}
+	// Is the attachment an heic?
+	if ( 'heic' !== pathinfo( $file_path, PATHINFO_EXTENSION ) ) {
+		// No.
+		return;
+	}
+	$imagick = new Imagick();
+	try {
+		if ( $imagick->readImage( $file_path ) ) {
+			$imagick->setImageFormat( 'webp' );
+			// Create a path to create a webp copy of the image.
+			$name       = basename( $file_path, '.heic' ) . '.webp';
+			$upload_dir = wp_upload_dir();
+			$imagick->writeImage( $upload_dir['path'] . DIRECTORY_SEPARATOR . $name );
+			$copy_post_id = media_sideload_image( $upload_dir['url'] . '/' . $name, 0, get_the_title( $post_id ), 'id' );
+			if ( ! is_wp_error( $copy_post_id ) ) {
+				update_post_meta( $copy_post_id, '_heic_support_copy_of', $post_id );
+				update_post_meta( $post_id, '_heic_support_copy_of', $copy_post_id );
+			} else {
+				error_log( '[HEIC Support] ' . $copy_post_id->get_error_message() );
+			}
+		}
+	} catch ( ImagickException $ie ) {
+		// "Fatal error: Uncaught ImagickException: no decode delegate for this image format `HEIC'".
+		error_log( '[HEIC Support] Failed to create copy of ' . $file_path . ', ' . $ie->getMessage() );
+	}
+}
+add_action( 'add_attachment', 'heic_support_create_webp_copy', 10, 1 );
+
+// - filter requests for .heic images for the substitute URLs and paths
+
+
+// SETTINGS PAGE.
+
 /**
  * heic_support_add_menu
  *
